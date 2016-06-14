19b6290978ba9288f667d06a119cfb890a90f33c
Hauke Bartsch <hbartsch@ucsd.edu>
2016-06-14 13:55:48 -0400

    get an output file

--- REDCap2NDA/redcap2ndar.sh
+++ REDCap2NDA/redcap2ndar.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
-import json
+import json, csv
 import sys, os.path
-
-
-
 
 if __name__ == "__main__":
 
@@ -24,15 +21,43 @@ if __name__ == "__main__":
             # define A as REDCap, B as NDA
             if os.path.isfile(mapfname): 
                 with open(mapfname) as data_file:
-                    map = json.loads(data_file.read())
+                    mapR2N = json.loads(data_file.read())
             else:
-                map = []
+                mapR2N = []
             print ("use the following map now: ")
-            print  json.dumps(map, sort_keys=True, indent=4, separators=(',', ': '))
+            print  json.dumps(mapR2N, sort_keys=True, indent=4, separators=(',', ': '))
 
             # lets go through and create the mapping
-            # for each entry in data
+            # for each entry in data (array of objects)
+            cannotConvert = []
+            erg=list()
+            for item in data:
+                # which of the fields can we convert?
+                # list of keys from REDCap
+                m1=list(map((lambda x: x['nameA']), mapR2N))
+                # matching list of keys from NDA
+                m2=list(map((lambda x: x['nameB']), mapR2N))
+                o={}
+                for (key, value) in item.items():
+                    try:
+                        i=m1.index(key)
+                        o[m1[i]].append( m2[i] )
+                    except:
+                        cannotConvert.append(key)
+                erg.append(o)
 
+            #print("Can convert these keys: \n")
+            #print json.dumps(erg, sort_keys=True, indent=4, separators=(',', ': '))
+            with open(output, 'w') as csvfile:
+                fieldnames = ['ElementName', 'DataType', 'Size', 'Required', 'ElementDescription', 'ValueRange', 'Notes', 'Aliases']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                # write a row that identifies the dictionary?
+                writer.writeheader()
+                for value in erg:
+                    writer.writerow(value)
+
+            print("Cannot convert these keys: \n")
+            print json.dumps(list(set(cannotConvert)), sort_keys=True, indent=4, separators=(',', ': '))
     else:
         print "Usage: read <name of your instrument file from REDCap> <output file name>"
     sys.exit(2)
